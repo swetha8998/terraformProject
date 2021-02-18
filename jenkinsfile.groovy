48db37a0e7a65be59acf5f4091c2b1e9ffa0058d
SWETHA S <43613703+swetha8998@users.noreply.github.com>
2021-02-18 21:55:47 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,19 +1,25 @@
-node{
-    stage("checkout"){
-        checkout scm
-    }
-    stage("Terraform init"){
-       
-     sh "terraform init"
-    }
-   
-    stage("plan"){
-        
-        sh "terraform plan"
-    }
-    stage("terraform apply"){
-    
-    sh "terraform apply"
-    
-    }
+pipeline{
+   agent any
+  
+   environment {
+    AWS_ACCESS_KEY_ID     = credentials('aws_access_id')
+    AWS_SECRET_ACCESS_KEY = credentials('aws_secret_key')
 }
+   stages{
+       stage('git chechout'){
+           steps{
+             git branch: 'branch1', url: 'https://github.com/swetha8998/terraformProject.git'
+           }
+       }
+       stage('Terraform init'){
+           steps{
+               sh 'terraform init'
+           }
+       }
+        stage('Terraform apply'){
+           steps{
+               sh 'terraform apply --auto-approve'
+           }
+       }
+   }
+ }
